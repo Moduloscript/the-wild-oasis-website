eaefe3bc2023917256f1d41360ee865b0b41436b
moduloscript <maduemeka254@gmail.com>
2024-06-11 12:25:26 +0100

    authentication done ✅✅🚀

--- app/_components/ReservationForm.js
+++ app/_components/ReservationForm.js
@@ -1,20 +1,19 @@
-"use client"
+"use client";
 
 import { useReservation } from "./ReservationContext";
 
-
-function ReservationForm({ cabin }) {
-  const {range} = useReservation()
+function ReservationForm({ cabin, user }) {
+  const { range } = useReservation();
 
   // CHANGE
-  const {maxCapacity} = cabin;
+  const { maxCapacity } = cabin;
 
   return (
     <div className='scale-[1.01]'>
       <div className='bg-primary-800 text-primary-300 px-16 py-2 flex justify-between items-center'>
         <p>Logged in as</p>
 
-        {/* <div className='flex gap-4 items-center'>
+        <div className='flex gap-4 items-center'>
           <img
             // Important to display google profile images
             referrerPolicy='no-referrer'
@@ -23,7 +22,7 @@ function ReservationForm({ cabin }) {
             alt={user.name}
           />
           <p>{user.name}</p>
-        </div> */}
+        </div>
       </div>
 
       <form className='bg-primary-900 py-10 px-16 text-lg flex gap-5 flex-col'>
@@ -33,14 +32,13 @@ function ReservationForm({ cabin }) {
             name='numGuests'
             id='numGuests'
             className='px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm'
-            required
-          >
+            required>
             <option value='' key=''>
               Select number of guests...
             </option>
             {Array.from({ length: maxCapacity }, (_, i) => i + 1).map((x) => (
               <option value={x} key={x}>
-                {x} {x === 1 ? 'guest' : 'guests'}
+                {x} {x === 1 ? "guest" : "guests"}
               </option>
             ))}
           </select>
