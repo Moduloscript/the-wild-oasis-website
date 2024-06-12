2be7a2f867ccff811b7e483c25d02404d15f49d9
moduloscript <maduemeka254@gmail.com>
2024-06-12 01:21:14 +0100

    added middleware features in the build both "sign-in", & "sign-out" features

--- app/account/page.js
+++ app/account/page.js
@@ -1,12 +1,15 @@
+import { auth } from '../_lib/auth';
 export const metadata = {
   title: "Guest Area",
 };
 
-export default function Account() {
+export default async function Account() {
+  const session = await auth();
+  console.log(session)
   return (
     <div>
       <h2 className='font-semibold text-2xl text-accent-400 mb-2'>
-        Welcome, Modulo
+        Welcome, {session.user.name}
       </h2>
     </div>
   );
